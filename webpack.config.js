e3a1b716584eb1fb33c49681dad12641bf90c72d
Zoran Markovic <zoran.markovic@netcentric.biz>
2018-01-24 15:44:17 +0100

    Webpack 2 Basics Tutorial HTML + IMAGE LOADERS

--- webpack.config.js
+++ webpack.config.js
@@ -4,13 +4,14 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var extractPlugin = new ExtractTextPlugin({
     filename: 'main.css'
 });
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/js/app.js', // where to start, relative path
     output: {
         path: path.resolve(__dirname, 'dist'),  // create absolute path for output, __dirname give us current directory
-        filename: 'bundle.js',
-        publicPath: '/dist' // where to find assets
+        filename: 'bundle.js'
+        //publicPath: '/dist' // where to find assets inside index.html file, we need it because it was in root folder
     },
     module: {
         rules: [
@@ -38,13 +39,31 @@ module.exports = {
                         'sass-loader'
                     ]
                 })
+            },
+            {
+                test: /\.html$/,
+                use: ['html-loader']
+            },
+            {
+                test: /\.(jpg|png)$/,
+                use: {
+                    loader: 'file-loader', // google for webpack file loader options
+                    options: {
+                        name: '[name].[ext]', //keep original name and extension
+                        outputPath: 'img/',  // by default will go in dist folder
+                        //publicPath: 'img/' // need also to keep correct reference to new folder 'img'
+                    }
+                }
             }
         ]
     },
     plugins: [
-        extractPlugin // store css code in separate file
+        extractPlugin, // store css code in separate file
         // new webpack.optimize.UglifyJsPlugin({
         //     // ...
         // })
+        new HtmlWebpackPlugin({ //automatically inject css and js bundle
+            template: 'src/index.html'
+        })
     ]
 };
